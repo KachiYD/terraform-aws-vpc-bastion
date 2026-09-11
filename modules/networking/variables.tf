9074b1141768557d31e89142b2f9e7a4eb592cea
KachiYD <kachi.oragui@gmail.com>
2026-09-10 23:20:17 -0600

    style: terraform fmt -recursive

--- modules/networking/variables.tf
+++ modules/networking/variables.tf
@@ -1,11 +1,11 @@
 variable "vpc_config" {
   type = object({
     cidr_block = string
-    name = string
+    name       = string
   })
 
   validation {
-    condition = can(cidrnetmask(var.vpc_config.cidr_block))
+    condition     = can(cidrnetmask(var.vpc_config.cidr_block))
     error_message = "The cidr_block config option must contain a valid CIDR block"
   }
 }
@@ -13,13 +13,13 @@ variable "vpc_config" {
 variable "subnet_config" {
   type = map(object({
     cidr_block = string
-    public = optional(bool, false)
-    az = string
+    public     = optional(bool, false)
+    az         = string
   }))
 
   validation {
     condition = alltrue([
-        for config in values(var.subnet_config): can(cidrnetmask(config.cidr_block))
+      for config in values(var.subnet_config) : can(cidrnetmask(config.cidr_block))
     ])
     error_message = "The cidr_block config option must contain a valid CIDR block"
   }
