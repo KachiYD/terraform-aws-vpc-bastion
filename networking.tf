890398c1d506349d57966c2ad10183c9cf250abf
Kachi Oragui <kachioragui@Kachis-Mac-mini.local>
2026-09-10 23:17:28 -0600

    chore: updated module declaration and fixed local variable calls

--- networking.tf
+++ networking.tf
@@ -5,4 +5,12 @@ module "vpc" {
     cidr_block = "10.0.0.0/16"
     name = "Main VPC"
   }
+
+  subnet_config = {
+    public_sub_1 = {
+        cidr_block = "10.0.0.0/24"
+        az = "ca-central-1a"
+        public = true
+    }
+  }
 }
