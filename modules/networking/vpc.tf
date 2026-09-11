a507eec7fc2ccc5de15b241541839549b99ea9ea
Kachi Oragui <kachioragui@Kachis-Mac-mini.local>
2026-09-10 22:35:12 -0600

    feat: added vpc resource creation and az data block

---
+++ modules/networking/vpc.tf
@@ -0,0 +1,15 @@
+locals {
+  selected_azs = slice(data.aws_availability_zones.available.names, 0, 2)
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "Main VPC"
+  }
+}
