e9bd964f7d19670afffb23701d4ba1e480e407ad
Kachi Oragui <kachioragui@Kachis-Mac-mini.local>
2026-09-12 22:13:59 -0600

    feat: IGW, public and private route tables, and NAT gateway

--- modules/networking/vpc.tf
+++ modules/networking/vpc.tf
@@ -1,5 +1,13 @@
 locals {
   selected_azs = slice(data.aws_availability_zones.available.names, 0, 2)
+
+  public_subnets = {
+    for key, config in var.subnet_config : key => config if config.public
+  }
+
+  private_subnets = {
+    for key, config in var.subnet_config : key => config if !config.public
+  }
 }
 
 data "aws_availability_zones" "available" {
@@ -35,5 +43,69 @@ resource "aws_subnet" "this" {
       [${join(", ", local.selected_azs)}]
       EOT
     }
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  count = length(local.public_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "Main IGW"
+  }
+}
+
+resource "aws_route_table" "public_rtb" {
+  vpc_id = aws_vpc.this.id
+  count = length(local.public_subnets) > 0 ? 1 : 0
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this[0].id
+  }
+
+  tags = {
+    Name = "Public RTB"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each = local.public_subnets
+
+  subnet_id = aws_subnet.this[each.key].id
+
+  route_table_id = aws_route_table.public_rtb[0].id
+}
+
+resource "aws_route_table" "private_rtb" {
+  vpc_id = aws_vpc.this.id
+  count = length(local.private_subnets) > 0 ? 1 : 0
+
+  tags = {
+    Name = "Private RTB"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each = local.private_subnets
+
+  subnet_id = aws_subnet.this[each.key].id
+
+  route_table_id = aws_route_table.private_rtb[0].id
+}
+
+resource "aws_eip" "elastic_ip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.elastic_ip.id
+
+  for_each = local.public_subnets
+
+  subnet_id = aws_subnet.this[each.key].id
+
+  tags = {
+    Name = "Main NAT Gateway"
   }
 }
