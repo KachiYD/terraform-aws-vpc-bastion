87097ae6555fa551cfcc7575b0cc495f25eceac2
Kachi Oragui <kachioragui@Kachis-Mac-mini.local>
2026-09-17 22:53:20 -0600

    feat: updated the gitignore file to include tfplan files

--- modules/networking/asg.tf
+++ modules/networking/asg.tf
@@ -47,7 +47,7 @@ resource "aws_vpc_security_group_ingress_rule" "app_traffic" {
   ip_protocol       = "tcp"
 }
 
-resource "aws_vpc_security_group_egress_rule" "name" {
+resource "aws_vpc_security_group_egress_rule" "app_all" {
   security_group_id = aws_security_group.app.id
   description       = "All outbound"
   cidr_ipv4         = "0.0.0.0/0"
