b5f17818b4153610a42346603bb527f9fd9ca000
Kachi Oragui <kachioragui@Kachis-Mac-mini.local>
2026-09-17 22:21:22 -0600

    feat: modified module outputs

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,3 +1,15 @@
-output "public_subnet_id" {
-  value = aws_subnet.this["public_sub_1"].id
+output "vpc_id" {
+    value = aws_vpc.this.id
+}
+
+output "vpc_cidr_block" {
+    value = aws_vpc.this.cidr_block
+}
+
+output "public_subnet_ids" {
+  value = [ for key, _ in local.public_subnets : aws_subnet.this[key].id ] 
+}
+
+output "private_subnet_ids" {
+  value = [ for key, _ in local.private_subnets : aws_subnet.this[key].id ] 
 }
